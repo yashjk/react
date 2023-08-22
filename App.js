877b627081d59953dc23a69aa8c794caadb40966
yash <injose.joshi@gmail.com>
2023-08-22 14:24:21 +0530

    [001] Add lesson 1 practice code

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading1 = React.createElement("h1", { id: "heading1" }, "Heading 1");
+const heading2 = React.createElement("h1", { id: "heading2" }, "Heading 2");
+
+const title = React.createElement("div", { id: "title" }, [heading1, heading2]);
+const container = React.createElement("div", { id: "container" }, [
+	title,
+	title,
+]);
+
+const container2 = React.createElement("div", { id: "container" }, [
+	title,
+	title,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render([container, container2]);
