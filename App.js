45835d14397f10f2cbd15ae8a3660992d33b7612
yash <injose.joshi@gmail.com>
2023-08-24 16:03:37 +0530

    [02] Add lesson 02

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 const heading1 = React.createElement("h1", { id: "heading1" }, "Heading 1");
 const heading2 = React.createElement("h1", { id: "heading2" }, "Heading 2");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const title = React.createElement("div", { id: "title" }, [heading1, heading2]);
 const container = React.createElement("div", { id: "container" }, [
